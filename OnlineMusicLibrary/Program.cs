c0ed4c75c472784264d16391e501d8d8818c6194
ConfiG <cgytrus@cgyt.ru>
2023-11-04 18:37:07 +0300

    swag

--- OnlineMusicLibrary/Program.cs
+++ OnlineMusicLibrary/Program.cs
@@ -12,6 +12,20 @@ using File = System.IO.File;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+    options.SchemaGeneratorOptions.SchemaIdSelector = modelType => {
+        string parent = modelType.DeclaringType is null ? "" :
+            options.SchemaGeneratorOptions.SchemaIdSelector(modelType.DeclaringType);
+
+        string generic = modelType.IsConstructedGenericType ? modelType.GetGenericArguments()
+            .Select(genericArg => options.SchemaGeneratorOptions.SchemaIdSelector(genericArg))
+            .Aggregate((previous, current) => $"{previous}{current}") : "";
+
+        return $"{parent}{generic}{modelType.Name.Replace("[]", "Array").Split('`').First()}";
+    };
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
@@ -26,26 +40,33 @@ builder.Services.AddCors(options => {
 
 WebApplication app = builder.Build();
 
+app.UsePathBase("/music");
+
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseCors();
 
-RouteGroupBuilder baseGroup = app.MapGroup("/music/v1");
+RouteGroupBuilder baseGroup = app.MapGroup("/v1");
 
 RouteGroupBuilder userGroup = baseGroup.MapGroup("/user");
 userGroup.MapGet("/verify", async (HttpContext ctx, ApplicationDbContext db) =>
-    await TryAuthorize(ctx, db) is null ? Results.Text("Invalid token") : Results.Text(""));
+    await TryAuthorize(ctx, db) is null ? Results.Text("Invalid token") : Results.Text("")).WithOpenApi();
 userGroup.MapGet("/{username}/tracks", (ApplicationDbContext db, string username) => {
     return Results.Ok(db.tracks
         .Where(x => x.username == username)
         .AsEnumerable()
         .Select(x => new Track.GetDto(x))
         .ToArray());
-});
+}).WithOpenApi();
 userGroup.MapGet("/{username}/playlists", async (ApplicationDbContext db, string username) => {
     return Results.Ok(await db.playlists
         .Where(x => x.username == username)
         .Select(x => x.id)
         .ToArrayAsync());
-});
+}).WithOpenApi();
 
 RouteGroupBuilder trackGroup = baseGroup.MapGroup("/track");
 trackGroup.MapPost("/", async (HttpContext ctx, ApplicationDbContext db, Track.CreateDto input) => {
@@ -57,7 +78,7 @@ trackGroup.MapPost("/", async (HttpContext ctx, ApplicationDbContext db, Track.C
     db.tracks.Add(track);
     await db.SaveChangesAsync();
     return Results.Created($"/track/{track.id}", new Track.GetDto(track));
-});
+}).WithOpenApi();
 trackGroup.MapPut("/{id}", async (HttpContext ctx, ApplicationDbContext db, uint id, Track.UpdateDto input) => {
     User? user = await TryAuthorize(ctx, db);
     if (user is null)
@@ -79,23 +100,23 @@ trackGroup.MapPut("/{id}", async (HttpContext ctx, ApplicationDbContext db, uint
         File.Delete(prevArtPart);
 
     return Results.NoContent();
-});
+}).WithOpenApi();
 trackGroup.MapGet("/{id}", async (ApplicationDbContext db, uint id) =>
     await db.tracks.FindAsync(id) is { } track ?
         Results.Ok(new Track.GetDto(track)) :
-        Results.NotFound());
+        Results.NotFound()).WithOpenApi();
 trackGroup.MapGet("/{id}/art", async (ApplicationDbContext db, uint id) =>
     await db.tracks.FindAsync(id) is { } track ?
         track.HasArt() ?
             Results.File(track.artPath, "media/jpeg", Path.GetFileName(track.artPath)) :
             Results.NotFound() :
-        Results.NotFound());
+        Results.NotFound()).WithOpenApi();
 trackGroup.MapGet("/{id}/lyrics", async (ApplicationDbContext db, uint id) =>
     await db.tracks.FindAsync(id) is { } track ?
         track.lyrics is { } lyrics ?
             Results.Text(lyrics, "text/plain") :
             Results.NotFound() :
-        Results.NotFound());
+        Results.NotFound()).WithOpenApi();
 trackGroup.MapGet("/{id}/download", async (HttpContext ctx, ApplicationDbContext db, uint id) => {
     User? user = await TryAuthorize(ctx, db);
     if (user is null)
@@ -155,7 +176,7 @@ trackGroup.MapGet("/{id}/download", async (HttpContext ctx, ApplicationDbContext
         File.Delete(path);
 
     return Results.Empty;
-});
+}).WithOpenApi();
 trackGroup.MapDelete("/{id}", async (HttpContext ctx, ApplicationDbContext db, uint id) => {
     User? user = await TryAuthorize(ctx, db);
     if (user is null)
@@ -177,7 +198,7 @@ trackGroup.MapDelete("/{id}", async (HttpContext ctx, ApplicationDbContext db, u
         File.Delete(prevArtPath);
 
     return Results.NoContent();
-});
+}).WithOpenApi();
 
 RouteGroupBuilder playlistGroup = baseGroup.MapGroup("/playlist");
 playlistGroup.MapPost("/", async (HttpContext ctx, ApplicationDbContext db, Playlist.CreateDto input) => {
@@ -189,7 +210,7 @@ playlistGroup.MapPost("/", async (HttpContext ctx, ApplicationDbContext db, Play
     db.playlists.Add(playlist);
     await db.SaveChangesAsync();
     return Results.Created($"/playlist/{playlist.id}", Playlist.GetDto.From(db, playlist));
-});
+}).WithOpenApi();
 playlistGroup.MapPut("/{id}", async (HttpContext ctx, ApplicationDbContext db, uint id, Playlist.UpdateDto input) => {
     User? user = await TryAuthorize(ctx, db);
     if (user is null)
@@ -205,11 +226,11 @@ playlistGroup.MapPut("/{id}", async (HttpContext ctx, ApplicationDbContext db, u
     await db.SaveChangesAsync();
 
     return Results.NoContent();
-});
+}).WithOpenApi();
 playlistGroup.MapGet("/{id}", async (ApplicationDbContext db, uint id) =>
     await db.playlists.FindAsync(id) is { } playlist ?
         Results.Ok(await Playlist.GetDto.From(db, playlist)) :
-        Results.NotFound());
+        Results.NotFound()).WithOpenApi();
 playlistGroup.MapDelete("/{id}", async (HttpContext ctx, ApplicationDbContext db, uint id) => {
     User? user = await TryAuthorize(ctx, db);
     if (user is null)
@@ -224,7 +245,7 @@ playlistGroup.MapDelete("/{id}", async (HttpContext ctx, ApplicationDbContext db
     await db.SaveChangesAsync();
 
     return Results.NoContent();
-});
+}).WithOpenApi();
 
 app.Run();
 
